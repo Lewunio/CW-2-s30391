a655c6ed882df16fd0eb0e2138fc85b0b8666261
s30391 <s30391@pjwstk.edu.pl>
2025-03-19 18:55:29 +0100

    Kontener na wode

--- Kontenery/Kontenery/Program.cs
+++ Kontenery/Kontenery/Program.cs
@@ -5,16 +5,16 @@
 abstract class Kontener
 {
     private static int Counter = 1;
-    private double MasaLadunku { get; set; }
-    private double Wysokosc { get; }
-    private double MasaWlasna { get; }
-    private double Glebokosc { get; }
-    private string NumerSeryjny { get; }
-    private double MaxLadownosc { get; }
+    protected double MasaLadunku { get; set; }
+    protected double Wysokosc { get; }
+    protected double MasaWlasna { get; }
+    protected double Glebokosc { get; }
+    protected string NumerSeryjny { get; }
+    protected double MaxLadownosc { get; }
 
-    public Kontener(double masaLadunku, double wysokosc, double masaWlasna, double glebokosc, double maxLadownosc, string typ)
+    public Kontener(double wysokosc, double masaWlasna, double glebokosc, double maxLadownosc, string typ)
     {
-        MasaLadunku = masaLadunku;
+       
         Wysokosc = wysokosc;
         MasaWlasna = masaWlasna;
         Glebokosc = glebokosc;
@@ -23,6 +23,40 @@ abstract class Kontener
     }
     public abstract void Laduj(double waga);
     public abstract void Rozladuj();
+}
+class KontenerNaPlyny : Kontener, IHazardNotifier
+{
+    protected bool CzyNiebezpieczne { get; set; }
+
+    public KontenerNaPlyny(double wysokosc, double masaWlasna, double glebokosc, double maxLadownosc, bool czyNiebezpieczne)
+        : base(wysokosc,masaWlasna,glebokosc,maxLadownosc,"L")
+    {
+        CzyNiebezpieczne = czyNiebezpieczne;
+    }
+
+    public void NotifyHazard(string message)
+    {
+        Console.WriteLine($"ALERT! {message}");
+    }
+
+    public override void Laduj(double waga)
+    {
+        double limit;
+        if (CzyNiebezpieczne) limit = MaxLadownosc * 0.5;
+        else limit = MaxLadownosc * 0.9;
+
+        if(waga > limit)
+        {
+            NotifyHazard($"Próba przeładowania kontenera {NumerSeryjny}");
+            throw new Exception("Overfill Exception");
+        }
+        MasaLadunku = waga;
+    }
+
+    public override void Rozladuj()
+    {
+        MasaLadunku = 0;
+    }
 }
 public class Program
 {
