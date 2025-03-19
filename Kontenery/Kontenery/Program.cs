3f57297d907e47e73953df60cb7a627b348b1262
s30391 <s30391@pjwstk.edu.pl>
2025-03-19 17:32:23 +0100

    Interface i Klasa Kontener

--- Kontenery/Kontenery/Program.cs
+++ Kontenery/Kontenery/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿interface IHazardNotifier
+{
+    void NotifyHazard(string message);
+}
+class Kontener
+{
+    private double Masa { get; set; }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
