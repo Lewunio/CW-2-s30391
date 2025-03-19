75986b1c298bdbd8c6af8368e38dc54f34a53a04
s30391 <s30391@pjwstk.edu.pl>
2025-03-19 19:05:41 +0100

    Kontener na gaz

--- Kontenery/Kontenery/Program.cs
+++ Kontenery/Kontenery/Program.cs
@@ -58,6 +58,35 @@ class KontenerNaPlyny : Kontener, IHazardNotifier
         MasaLadunku = 0;
     }
 }
+class KontenerNaGaz : Kontener, IHazardNotifier
+{
+    protected double Cisnienie;
+    public KontenerNaGaz(double wysokosc, double masaWlasna, double glebokosc, double maxLadownosc, double cisnienie) 
+        : base(wysokosc, masaWlasna, glebokosc, maxLadownosc, "G")
+    {
+        Cisnienie = cisnienie;
+    }
+
+    public override void Laduj(double waga)
+    {
+        if (waga > MaxLadownosc)
+        {
+            NotifyHazard($"Próba przeładowania kontenera {NumerSeryjny}");
+            throw new Exception("Overfill Exception");
+        }
+        MasaLadunku = waga;
+    }
+
+    public void NotifyHazard(string message)
+    {
+        Console.WriteLine($"ALERT! {message}");
+    }
+
+    public override void Rozladuj()
+    {
+        MasaLadunku *= 0.05;
+    }
+}
 public class Program
 {
     public static void Main(string[] args)
