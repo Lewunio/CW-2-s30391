e6bcdb780e430986aa18348dc91ff66e114ff1a9
s30391 <s30391@pjwstk.edu.pl>
2025-03-19 23:29:05 +0100

    Poczatek statku

--- Kontenery/Kontenery/Program.cs
+++ Kontenery/Kontenery/Program.cs
@@ -134,8 +134,10 @@ class KontenerChlodniczy : Kontener
 }
 class Statek
 {
-    protected List<Kontener> Zaladunek { get; }
+    protected List<Kontener> Zaladunek { get; } = new();
     protected double MaxPredkosc { get; }
+    protected int MaxKontenerow { get; }
+
 
 }
 public class Program
