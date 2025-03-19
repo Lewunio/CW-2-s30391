80c2c6d2e4fd22116616a10b58bd4221ad309ea5
s30391 <s30391@pjwstk.edu.pl>
2025-03-19 17:32:50 +0100

    Interface i Klasa Kontener - teraz

--- Kontenery/Kontenery/Program.cs
+++ Kontenery/Kontenery/Program.cs
@@ -2,9 +2,27 @@
 {
     void NotifyHazard(string message);
 }
-class Kontener
+abstract class Kontener
 {
-    private double Masa { get; set; }
+    private static int Counter = 1;
+    private double MasaLadunku { get; set; }
+    private double Wysokosc { get; }
+    private double MasaWlasna { get; }
+    private double Glebokosc { get; }
+    private string NumerSeryjny { get; }
+    private double MaxLadownosc { get; }
+
+    public Kontener(double masaLadunku, double wysokosc, double masaWlasna, double glebokosc, double maxLadownosc, string typ)
+    {
+        MasaLadunku = masaLadunku;
+        Wysokosc = wysokosc;
+        MasaWlasna = masaWlasna;
+        Glebokosc = glebokosc;
+        MaxLadownosc = maxLadownosc;
+        NumerSeryjny = $"KON-{typ}-{Counter++}";
+    }
+    public abstract void Laduj(double waga);
+    public abstract void Rozladuj();
 }
 public class Program
 {
